1d5ba02b623e4a0633e7dfb7eb7b4789451e772d
Gabriel Alejandro Nuñez <135710459+Gabriel-227@users.noreply.github.com>
2025-02-25 21:48:42 -0300

    tipos primitivos

---
+++ tipos-primitivos.js
@@ -0,0 +1,26 @@
+//Tipos Primitivos
+
+//number
+4;
+0;
+
+//string
+"Hola Mundo";
+'Hola Mundo';
+
+//boolean
+true;
+false;
+
+//nulo y undefined
+null;
+undefined;
+
+//null, undefined, false, 0 Todos son valores falsy
+console.log(null===undefined); 
+
+if (true) {
+    console.log("cumple");
+} else {
+    console.log("No cumple");
+}
