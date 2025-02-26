3c58c14660ab0cf1ac4bb08e4fff9d81925d22a1
Gabriel Alejandro Nuñez <135710459+Gabriel-227@users.noreply.github.com>
2025-02-26 01:44:19 -0300

    nootaciones

---
+++ notacion-list-objetos.js
@@ -0,0 +1,31 @@
+// Notacion
+// ; delimitar el final de una linea
+const x = 1;
+x + 4;
+
+// . para acceder a propiedades de un objeto    para acceder a los atributos
+// movil.anchura
+
+// [] para acceder a elementos de un array, listas arreglos
+const lista = [1, 2, 3];
+lista[0];
+console.log(lista[0]);
+
+// () para llamar a una funcion
+function suma(a, b) {
+    //Se escribiria todo el codigo de la funcion
+    // return a + b;
+}
+
+// {} para definir bloques de codigo - para los objetos funciones y estructura de control
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if (true) {
+    // Todo lo que haya entre llaves
+
+
+    const constante2 = 'Hola';
+}
